105558a05e9bb20739f554153befea52eac53757
YUKI MORI <yukimori@YUKInoMacBook-Pro.local>
2016-12-23 10:49:52 +0900

    テストアラート

--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@ $(function(){
     $('a img').hover(function(){
     $(this).attr('src', $(this).attr('src').replace('_off', '_on'));
     }, function(){
+
+        alert("test alert");
+
         if (!$(this).hasClass('currentPage')) {
         $(this).attr('src', $(this).attr('src').replace('_on', '_off'));
         }
