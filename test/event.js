3146d0ef2f3b8f14414a8d449e77c4c581c98971
FOREST_windows <tobe77537@gmail.com>
2017-04-17 05:08:24 +0900

    テーブル追加

--- test/event.js
+++ test/event.js
@@ -1,17 +1,28 @@
 $(window).on('load',function(){
     var url = getRandomMovieUrl();
     if(url === undefined){
-        url = "http://203.130.55.101/ws.acgvideo.com/c/83/16358886-1-hd.mp4?wsTime=1492371389&platform=pc&wsSecret2=574a8fe3065e031c5155b609198ad90f&oi=1924520460&rate=1350&wshc_tag=0&wsts_tag=58f396b2&wsid_tag=72b5da0c&wsiphost=ipdbm";
-        localStorage.setItem("start0_" + getDate(), url);
-        url = "http://ws.acgvideo.com/8/52/16358887-1-hd.mp4?wsTime=1492373620&platform=pc&wsSecret2=0ba8e3a7a19c4666566c79c3878b4f4a&oi=1924520460&rate=1350"
-        localStorage.setItem("start1_" + getDate(), url);
-        url = "http://ws.acgvideo.com/c/68/16239234-1-hd.mp4?wsTime=1492381819&platform=pc&wsSecret2=64d228244cab31bb2453ec20617bb10d&oi=1924520460&rate=1350"
-        localStorage.setItem("start2_" + getDate(), url);
-        url = "http://ws.acgvideo.com/6/5e/16239418-1-hd.mp4?wsTime=1492381918&platform=pc&wsSecret2=f1bd27115793a83fd66acbf64d66de1f&oi=1924520460&rate=1350"
-        localStorage.setItem("start3_" + getDate(), url);
+        var movieList = [
+            "http://ws.acgvideo.com/5/8f/16324689-1-hd.mp4?wsTime=1492383152&platform=pc&wsSecret2=26f5d985ec487472675fb4973462c344&oi=1924520460&rate=1350"
+            ,"http://ws.acgvideo.com/b/c2/16324690-1-hd.mp4?wsTime=1492383205&platform=pc&wsSecret2=a521625c54518089225fbf89315c0bb6&oi=1924520460&rate=1350"
+            ,"http://ws.acgvideo.com/c/68/16239234-1-hd.mp4?wsTime=1492382425&platform=pc&wsSecret2=1c7e92793842b5257d8f4bf4d95a4672&oi=1924520460&rate=1350"
+            ,"http://ws.acgvideo.com/6/5e/16239418-1-hd.mp4?wsTime=1492382482&platform=pc&wsSecret2=135fc75eb6639505c39e9ebb82a5d5d3&oi=1924520460&rate=1350"
+            ,"http://ws.acgvideo.com/8/e9/16315681-1-hd.mp4?wsTime=1492382700&platform=pc&wsSecret2=50f23fa769b9cb19185970510dc022b9&oi=1924520460&rate=1350"
+            ,"http://ws.acgvideo.com/d/d8/15957515-1-hd.mp4?wsTime=1492385470&platform=pc&wsSecret2=7f604d83e0b1cdeb8bbe3411d418b4c8&oi=1924520460&rate=1350"
+            ,"http://ws.acgvideo.com/d/70/15537449-1-hd.mp4?wsTime=1492385782&platform=pc&wsSecret2=f79cbc5635745e7e688c32a3244299a6&oi=1924520460&rate=1350"
+        ];
+
+        var count = 0;
+        for (var startUrl of movieList){
+            localStorage.setItem("start" + count.toString() + "_" + getDate(), startUrl);
+            count++;
+        }
     }
 
+    // Video
     playMovie(url);
+    createTable();
+
+    // Event
     document.getElementById('video').addEventListener("ended", updateState, true);
 });
 
@@ -43,13 +54,25 @@ function hasMovieUrl(url){
 function getRandomMovieUrl(){
     // localstoge
     if(localStorage.length > 0){
-        var key = localStorage.key(getRandomArbitary(0, localStorage.length));
-        return localStorage.getItem(key);
+        return getLocalStorageItemVal(getRandomArbitary(0, localStorage.length))
     }
 }
 
 function getRandomArbitary(min, max) {
     return parseInt(Math.random() * (max - min) + min);
+}
+
+function getLocalStorageItemVal(index){
+    return localStorage.getItem(localStorage.key(index));
+}
+
+function createTable(){
+    var table = document.getElementById("movieTable");
+
+    for (var i = 0; i < localStorage.length; i++) {
+        var cell = table.insertRow(-1).insertCell(-1);
+        cell.innerText = getLocalStorageItemVal(i).split("?")[0];
+    }
 }
 
 function playMovie(src){
