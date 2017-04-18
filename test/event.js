05d9fd7a7a3e1dce3844da2cb535edcb2ce63225
FOREST_windows <tobe77537@gmail.com>
2017-04-19 01:56:34 +0900

    Youtube対応

--- test/event.js
+++ test/event.js
@@ -1,37 +1,88 @@
 $(window).on('load',function(){
-    var url = getRandomMovieUrl();
-    if(url === undefined){
-        var movieList = [
-            "http://ws.acgvideo.com/5/8f/16324689-1-hd.mp4?wsTime=1492383152&platform=pc&wsSecret2=26f5d985ec487472675fb4973462c344&oi=1924520460&rate=1350"
-            ,"http://ws.acgvideo.com/b/c2/16324690-1-hd.mp4?wsTime=1492383205&platform=pc&wsSecret2=a521625c54518089225fbf89315c0bb6&oi=1924520460&rate=1350"
-            ,"http://ws.acgvideo.com/c/68/16239234-1-hd.mp4?wsTime=1492382425&platform=pc&wsSecret2=1c7e92793842b5257d8f4bf4d95a4672&oi=1924520460&rate=1350"
-            ,"http://ws.acgvideo.com/6/5e/16239418-1-hd.mp4?wsTime=1492382482&platform=pc&wsSecret2=135fc75eb6639505c39e9ebb82a5d5d3&oi=1924520460&rate=1350"
-            ,"http://ws.acgvideo.com/d/d8/15957515-1-hd.mp4?wsTime=1492385470&platform=pc&wsSecret2=7f604d83e0b1cdeb8bbe3411d418b4c8&oi=1924520460&rate=1350"
-            ,"http://ws.acgvideo.com/d/70/15537449-1-hd.mp4?wsTime=1492385782&platform=pc&wsSecret2=f79cbc5635745e7e688c32a3244299a6&oi=1924520460&rate=1350"
-            ,"http://ws.acgvideo.com/6/bc/15940645-1-hd.mp4?wsTime=1492392952&platform=pc&wsSecret2=fca3479fed902f09f479905a66ca556c&oi=1924520460&rate=1350"
-
-        ];
-
-        var count = 0;
-        for (var startUrl of movieList){
-            localStorage.setItem("start" + count.toString() + "_" + getDate(), startUrl);
-            count++;
-        }
+    if(getRandomMovieUrl() === undefined){
+        defaultSetMovie();
     }
     // Event
     document.getElementById('video').addEventListener("ended", updateState, false);
     document.getElementById('video').addEventListener("error", updateState, false);
     document.getElementById('video').addEventListener("waiting", updateState, false);
     document.getElementById('video').addEventListener("timeupdate", updateState, false);
+    document.getElementById('video').volume = 0.5;
     
-    // Video
-    playMovie(url);
+    // Youtube
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    // テーブル作成
     createTable();
+    // KeyboradEnvet
+    keyboradEnvet();
+
+    // Youtube設定
+    player = new YT.Player('player', {
+        playerVars: {
+            autoplay: 1,
+            controls: 1,
+            showinfo: 0
+        }
+        ,events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+        }
+    });
+
+    $.ajax({
+        type: 'GET',
+        url: 'http://www.youtube.com/get_video_info?video_id=2hLZLnWZelk',
+        dataType: 'jsonp',
+        jsonp: 'callback',//コールバックパラメータ名の指定
+        //jsonpCallback: 'testCallback',//callback関数名を自分で指名した場合
+    })
+    .done(function(data){
+        console.log("success");
+            console.log(data);
+            // $.each(data,function(i, item){
+            //     $("#jsonp").append(item.formal  + ' <a href=" ' + item.url_pc + ' " target="_blank"> ' + item.url_pc +'</a><br />');
+            // });
+            
+            /*
+            for(var i=0; i < data.length; i++){
+                $("#jsonp").append(data[i].formal  + ' <a href=" ' + data[i].url_pc + ' " target="_blank"> ' + data[i].url_pc +'</a><br />');
+            }
+            */
+    })
+    .fail(function(data){
+        console.log("error");
+            $("#jsonp").append("エラーです");
+    });
 });
+
+function defaultSetMovie(){
+    var movieList = [
+        "http://ws.acgvideo.com/c/68/16239234-1-hd.mp4?wsTime=1492530217&platform=pc&wsSecret2=82a94b5f6706b91511f58639ff93e4f6&oi=1924520460&rate=1350"
+        ,"http://s3.amazonaws.com/ksr/assets/016/297/835/984404f51db3a3d9c1e6ab61547ed34b_original.mp3?"
+        ,"https://www.youtube.com/embed/2hLZLnWZelk?autoplay=1"
+        ,"https://ia601505.us.archive.org/10/items/GuP05_201704/GuP05.mp4"
+        ,"https://files.catbox.moe/ghz35z.mp3?"
+        ,"https://nodefiles.com/vidembed-qdaka8v7uf4u.mp4"
+    ];
+
+    var count = 0;
+    for (var startUrl of movieList){
+        localStorage.setItem("start" + count.toString() + "_" + getDate(), startUrl);
+        count++;
+    }
+}
 
 // LocalStorage Save
 function saveUrl(){
-    var url = document.getElementById("url").value;
+    var url = document.getElementById("urlTable").value;
+
+    if (url.length === 0) {
+       return; 
+    }
 
     if (hasMovieUrl(url)) {
         url = searchUrl(url);
@@ -43,6 +94,20 @@ function saveUrl(){
     if (document.getElementById("urlLoad").checked) {
         playMovie(url);
     } 
+
+    // テーブル作成
+    createTable();
+}
+
+// local storege date delete
+function reset(){
+    while(localStorage.length != 0){
+        localStorage.removeItem(localStorage.key(0));
+    }
+    defaultSetMovie();
+    playMovie(getRandomMovieUrl());
+    // テーブル作成
+    createTable();
 }
 
 function hasMovieUrl(url){
@@ -64,7 +129,6 @@ function searchUrl(hint){
 }
 
 function getRandomMovieUrl(){
-    // localstoge
     if(localStorage.length > 0){
         return getLocalStorageItemVal(getRandomArbitary(0, localStorage.length))
     }
@@ -80,6 +144,7 @@ function getLocalStorageItemVal(index){
 
 function createTable(){
     var table = document.getElementById("movieTable");
+    $("#movieTable tr").remove();
 
     for (var i = 0; i < localStorage.length; i++) {
         var cell = table.insertRow(-1).insertCell(-1);
@@ -90,11 +155,61 @@ function createTable(){
 function playMovie(src){
     var title = document.getElementById('title');
     var video = document.getElementById('video');
-    
+    var youtube = document.getElementById('player');
+
     title.innerHTML = src;
 
-    video.src = src;
-    video.play();
+    if(src.indexOf("youtu.be") > 0 || src.indexOf("www.youtube.com") > 0){
+        var matchList = [
+            "https://([a-zA-Z0-9\-_\.:@!~*'\(¥);\+$,]+)/([a-zA-Z0-9\-_\.:@!~*'\(¥);\+$,]+)/([a-zA-Z0-9\-_\.:@!~*'\(¥);\+$,]+)[?]{0,1}"
+            ,"https://([a-zA-Z0-9\-_\.:@!~*'\(¥);\+$,]+)/watch\[?]{1}v=([a-zA-Z0-9\-_\.:@!~*'\(¥);\+$,]+)"
+            ,"https://youtu.be/([a-zA-Z0-9\-_\.:@!~*'\(¥);\+$,]+)"
+        ];
+
+        var cnt = 0;
+        var youId = undefined;
+        for (item in matchList) {
+            var resultVal = src.match(matchList[item]);
+            
+            if (resultVal !== null) {
+                if (cnt === 0) {
+                    youId = resultVal[3];
+                }
+                else if (cnt === 1) {
+                    youId = resultVal[2];
+                }
+                else if (cnt === 2) {
+                    youId = resultVal[2];
+                }
+                break;
+            }
+            cnt++;
+        }
+
+        // URLの場合
+        if (youId === undefined ){
+             playMovie(getRandomMovieUrl());
+        }
+        
+        // Video Start
+        player.loadVideoById({
+            'videoId': youId,
+        });
+        
+        // None
+        video.pause();
+        video.style.display= "none";
+        youtube.style.display = "block";
+    } else {
+        // Video Start
+        video.src = src;
+        video.play();
+
+        // None
+        player.stopVideo();
+        youtube.style.display= "none"
+        video.style.display = "block";
+    }
 }
 
 /* 状態表示 */
@@ -107,9 +222,11 @@ function updateState(evt) {
 	}
 
 	if( video.error ) {
-        console.log(video.error);
-        console.log(document.getElementById('video').src);
-        playMovie(getRandomMovieUrl());
+        if (localStorage.length > 0) {
+            console.log(video.error);
+            console.log(document.getElementById('video').src);
+            playMovie(getRandomMovieUrl());
+        }
 	} else if( t == "loadstart" ) {
 	} else if( t == "loadeddata" ) {
 	} else if( t == "play" ) {
@@ -119,12 +236,77 @@ function updateState(evt) {
         playMovie(getRandomMovieUrl());
 	} else if( t == "waiting" ) {
         console.log("waiting");
-        video.currentTime = video.currentTime + 5.0;
-        video.currentTime = video.currentTime - 5.0;
-        // video.currentTime = video.currentTime - 1.0;
+        video.currentTime = video.currentTime + 10.0;
+        video.currentTime = video.currentTime - 10.0;
 	} else if( t == "timeupdate" ){
         console.log("timeupdate " + video.currentTime.toString());
     }
+}
+
+function onPlayerReady(event) {
+    initVolume();
+    // Start Video
+    playMovie(getRandomMovieUrl());
+}
+
+function initVolume(){
+    // ボリューム制御
+    player.setVolume(playerVol * 100);
+    video.volume = playerVol;
+}
+
+function setVolume(addNum){
+    
+    playerVol += addNum;
+
+    if (playerVol < 0){
+        playerVol = 0;
+    }
+    else if (playerVol > 1){
+        playerVol = 1;
+    }
+
+    player.setVolume(playerVol * 100);
+    video.volume = playerVol;
+}
+
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.ENDED) {
+        playMovie(getRandomMovieUrl());
+    }
+}
+
+
+function keyboradEnvet(){
+    document.onkeydown = function(event) {
+        var keyCode = false;
+    
+        if (event) {
+            if (event.keyCode) {
+                keyCode = event.keyCode;
+            } else if (event.which) {
+                keyCode = event.which;
+            }
+        }
+
+        // Space Key
+        if (keyCode === 32) {
+            
+        }
+        else if (keyCode === 37 || keyCode === 39) {
+             playMovie(getRandomMovieUrl());
+        }
+        else if (keyCode === 38) {
+            // ボリュームアップ
+            setVolume(playerAddVol);
+        }
+        else if (keyCode === 40) {
+            // ボリュームダウン
+            setVolume(-1 * playerAddVol);
+        }
+
+        console.log(keyCode);
+    };
 }
 
 function getDate(){
---
+++ test/yml.js
@@ -0,0 +1,3 @@
+var player = undefined;
+var playerVol = 0.3;
+var playerAddVol = 0.03;
